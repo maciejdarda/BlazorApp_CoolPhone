8f5a68711bb429cdc550ea35cc52795a2815914d
mdarda <maciejdarda@gmail.com>
2022-07-10 14:39:02 +0200

    Add ItemModel class

---
+++ BlazorApp_CoolPhone/Models/ItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlazorApp_CoolPhone.Models
+{
+    public class ItemModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
+
+        public bool Editable { get; set; } = false;
+    }
+}
