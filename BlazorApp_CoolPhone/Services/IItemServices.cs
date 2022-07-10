e1b4f8e71511b75c595cd194cd432e7ec790b526
mdarda <maciejdarda@gmail.com>
2022-07-10 15:19:29 +0200

    Add ItemServices and IItemServices classes

---
+++ BlazorApp_CoolPhone/Services/IItemServices.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApp_CoolPhone.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorApp_CoolPhone.Services
+{
+    public interface IItemServices
+    {
+        Task<IEnumerable<ItemModel>> GetReports();
+    }
+}
---
+++ BlazorApp_CoolPhone/Services/ItemServices.cs
@@ -0,0 +1,34 @@
+﻿using BlazorApp_CoolPhone.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorApp_CoolPhone.Services
+{
+    public class ItemServices : IItemServices
+    {
+        private readonly HttpClient _httpClient;
+        private readonly JsonSerializerOptions _options;
+
+        public ItemServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<IEnumerable<ItemModel>> GetReports()
+        {
+            var response = await _httpClient.GetAsync("reports");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            var items = JsonSerializer.Deserialize<List<ItemModel>>(content, _options);
+            return items;
+        }
+    }
+}
